e30bb302f84129fdb8e976e56035e887945b7a2a
bydens <bydens@gmail.com>
2015-10-21 14:10:26 +0300

    add products control

--- js/controllers/adminControllers.js
+++ js/controllers/adminControllers.js
@@ -1,5 +1,6 @@
 angular.module('sportsStoreAdmin')
   .constant('authUrl', 'http://localhost:2403/users/login')
+  .constant('ordersUrl', 'http://localhost:2403/orders')
   .controller('authCtrl', function($scope, $http, $location, authUrl) {
 
     $scope.authenticate = function(user, pass) {
@@ -15,18 +16,41 @@ angular.module('sportsStoreAdmin')
       });
     };
 
-  }).controller('mainCtrl', ['$scope', function($scope){
-    
-    $scope.screens = ['Products', 'Orders'];
+  }).controller("mainCtrl", function($scope) {
+
+    $scope.screens = ["Products", "Orders"];
     $scope.current = $scope.screens[0];
 
-    $scope.setScreen = function(index) {
+    $scope.setScreen = function (index) {
       $scope.current = $scope.screens[index];
     };
 
-    $scope.getScreen = function() {
-      return $scope.current == 'Products' ? '/views/adminProducts.html' :
-        '/views/adminOrders.html';
+    $scope.getScreen = function () {
+      return $scope.current == "Products"
+      ? "/views/adminProducts.html" : "/views/adminOrders.html";
     };
 
-  }]);
+  })
+  .controller("ordersCtrl", function ($scope, $http, ordersUrl) {
+    
+    $http.get(ordersUrl, {withCredentials : true})
+      .success(function (data) {
+        $scope.orders = data;
+      })
+      .error(function (error) {
+        $scope.error = error;
+      });
+
+    $scope.selectedOrder;
+
+    $scope.selectOrder = function(order) {
+      $scope.selectedOrder = order;
+      };
+    $scope.calcTotal = function(order) {
+      var total = 0;
+      for (var i = 0; i < order.products.length; i++) {
+        total += order.products[i].count * order.products[i].price;
+      }
+      return total;
+    };
+});
